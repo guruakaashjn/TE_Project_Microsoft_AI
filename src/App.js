d263568afeaee49f0ecdd65adf60422bf6ca7bca
guru24102001 <gurujnn@gmail.com>
2022-04-27 22:11:29 +0530

    Remove warning

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Concern from "./components/Concern";
+// import Concern from "./components/Concern";
 import Dataset from "./pages/Dataset";
 import Landing from "./pages/Landing";
 import Prevention from "./pages/Prevention";
-import Plotgraph from "./components/Plot";
-import StatewiseScatter from "./components/StatewiseScatter";
+// import Plotgraph from "./components/Plot";
+// import StatewiseScatter from "./components/StatewiseScatter";
 import { Routes, Route } from "react-router-dom";
 import Visualisation from "./pages/Visualisation";
 import Models from "./pages/Models";
