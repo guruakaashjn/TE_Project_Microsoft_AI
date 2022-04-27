4c36883ea638f221b44d9d6bc65a718986b40b67
Manigandan Kasimani <manigandankasimani@gmail.com>
2022-04-27 21:28:17 +0530

    Plastic Prediction graph add

--- src/pages/Models.js
+++ src/pages/Models.js
@@ -16,11 +16,11 @@ const Models = ({ home }) => {
 		years[year] = year;
 	}
 
-	const models = {
-		"CatBoost Regression": "CatBoost Regression",
-		// "Polynomial Regression": "Polynomial Regression",
-		"RandomForest Regression": "RandomForest Regression",
-	};
+	// const models = {
+	// 	"CatBoost Regression": "CatBoost Regression",
+	// 	// "Polynomial Regression": "Polynomial Regression",
+	// 	"RandomForest Regression": "RandomForest Regression",
+	// };
 
 	const states = [
 		"Andaman and Nicobar Islands",
@@ -68,17 +68,18 @@ const Models = ({ home }) => {
 	console.log(regions);
 
 	const [year, setYear] = useState(2020);
-	const [region, setRegion] = useState("");
-	const [actual, setActual] = useState("");
-	const [predicted, setPredicted] = useState("");
-	const [currentRegion, setCurrentRegion] = useState("");
-	console.log(currentRegion);
+	// const [region, setRegion] = useState("");
+	// const [actual, setActual] = useState("");
+	// const [predicted, setPredicted] = useState("");
+	// const [currentRegion, setCurrentRegion] = useState("");
+	// console.log(currentRegion);
 	// const [countryActual, setCountryActual] = useState("");
 	// const [countryPredicted, setCountryPredicted] = useState("");
 
 	const [rows, setRows] = useState({});
-	const [currentModel, setCurrentModel] = useState("RandomForest Regression");
+	// const [currentModel, setCurrentModel] = useState("RandomForest Regression");
 
+	// This useEffect gets the data from dataset to fill the table
 	useEffect(() => {
 		// console.log(year);
 		// console.log(region);
@@ -110,118 +111,156 @@ const Models = ({ home }) => {
 		console.log(rows);
 	}, [rows]);
 
-	useEffect(() => {
-		const url = "http://localhost:8000";
-		const config = {
-			"Content-Type": "application/json",
-			"Access-Control-Allow-Origin": "*",
-		};
+	// This useEffect returns all the data of the given region
+	// useEffect(() => {
+	// 	const url = "http://localhost:8000";
+	// 	const config = {
+	// 		"Content-Type": "application/json",
+	// 		"Access-Control-Allow-Origin": "*",
+	// 	};
 
-		try {
-			const getOneStateData = async () => {
-				console.log(region);
-				const response = await axios.get(
-					`${url}/api/get-data-state?state=${region}`,
-					config
-				);
-				if (response.status === 200) {
-					setCurrentRegion(response.data);
-					setActual({
-						x: response.data.year,
-						y: response.data.plastic,
-						type: "scatter",
-					});
-					console.log(response.data);
-				}
-			};
-			if (region) {
-				getOneStateData();
-			}
-		} catch (error) {
-			console.log(error.message);
-		}
-	}, [region]);
+	// 	try {
+	// 		const getOneStateData = async () => {
+	// 			console.log(region);
+	// 			const response = await axios.get(
+	// 				`${url}/api/get-data-state?state=${region}`,
+	// 				config
+	// 			);
+	// 			if (response.status === 200) {
+	// 				setCurrentRegion(response.data);
+	// 				setActual({
+	// 					x: response.data.year,
+	// 					y: response.data.plastic,
+	// 					type: "scatter",
+	// 				});
+	// 				console.log(response.data);
+	// 			}
+	// 		};
+	// 		if (region) {
+	// 			getOneStateData();
+	// 		}
+	// 	} catch (error) {
+	// 		console.log(error.message);
+	// 	}
+	// }, [region]);
 
-	useEffect(() => {
-		console.log(region);
-	}, [region]);
+	// useEffect(() => {
+	// 	console.log(region);
+	// }, [region]);
+
+	// useEffect(() => {
+	// 	const url = "http://localhost:8000";
+	// 	const config = {
+	// 		"Content-Type": "application/json",
+	// 		"Access-Control-Allow-Origin": "*",
+	// 	};
+	// 	try {
+	// 		const getOneStateDataOverYears = async () => {
+	// 			console.log(region);
+	// 			const response = await axios.get(
+	// 				`${url}/api/get-data/models/rf-state/?year=${year}`,
+	// 				config
+	// 			);
+	// 			console.log(response.data);
+	// 			if (response.status === 200) {
+	// 				setPredicted({
+	// 					values: response.data.data,
+	// 					labels: response.data.states,
+	// 					type: "pie",
+	// 				});
+	// 				// console.log(response.data);
+	// 			}
+	// 		};
+	// 		if (year) {
+	// 			getOneStateDataOverYears();
+	// 		}
+	// 	} catch (error) {
+	// 		console.log(error.message);
+	// 	}
+	// }, [year]);
 
 	// to get the predicted value of a state
-	useEffect(() => {
-		const url = "http://localhost:8000";
-		const config = {
-			"Content-Type": "application/json",
-			"Access-Control-Allow-Origin": "*",
-		};
+	// useEffect(() => {
+	// 	const url = "http://localhost:8000";
+	// 	const config = {
+	// 		"Content-Type": "application/json",
+	// 		"Access-Control-Allow-Origin": "*",
+	// 	};
 
-		try {
-			const getOneStateDataOverYears = async () => {
-				console.log(region);
-				const response = await axios.get(
-					`${url}/api/get-random-over?state=${region}&year=${year}`,
-					config
-				);
-				if (response.status === 200) {
-					setPredicted({
-						x: Object.keys(years),
-						y: response.data.predictedValue,
-						type: "scatter",
-					});
-					console.log(response.data);
-				}
-			};
-			if (region) {
-				getOneStateDataOverYears();
-			}
-		} catch (error) {
-			console.log(error.message);
-		}
-	}, [year, region]);
+	// 	try {
+	// 		const getOneStateDataOverYears = async () => {
+	// 			console.log(region);
+	// 			const response = await axios.get(
+	// 				`${url}/api/get-random-over?state=${region}&year=${year}`,
+	// 				config
+	// 			);
+	// 			if (response.status === 200) {
+	// 				setPredicted({
+	// 					x: Object.keys(years),
+	// 					y: response.data.predictedValue,
+	// 					type: "scatter",
+	// 				});
+	// 				console.log(response.data);
+	// 			}
+	// 		};
+	// 		if (region) {
+	// 			getOneStateDataOverYears();
+	// 		}
+	// 	} catch (error) {
+	// 		console.log(error.message);
+	// 	}
+	// }, [year, region]);
 
 	// State wise Catboost regression
 
-	useEffect(() => {
-		const url = "http://localhost:8000";
-		const config = {
-			"Content-Type": "application/json",
-			"Access-Control-Allow-Origin": "*",
-		};
+	// useEffect(() => {
+	// 	const url = "http://localhost:8000";
+	// 	const config = {
+	// 		"Content-Type": "application/json",
+	// 		"Access-Control-Allow-Origin": "*",
+	// 	};
 
-		try {
-			const getOneStateDataOverYearsCB = async () => {
-				console.log(region);
-				const response = await axios.get(
-					`${url}/api/get-cb-over?state=${region}&year=${year}`,
-					config
-				);
-				if (response.status === 200) {
-					setPredicted({
-						x: Object.keys(years),
-						y: response.data.predictedValue,
-						type: "scatter",
-					});
-					console.log(response.data);
-				}
-			};
-			if (currentModel === "CatBoost Regression") {
-				getOneStateDataOverYearsCB();
-			}
-		} catch (error) {
-			console.log(error.message);
-		}
-	}, [year, region, currentModel]);
+	// 	try {
+	// 		const getOneStateDataOverYearsCB = async () => {
+	// 			console.log(region);
+	// 			const response = await axios.get(
+	// 				`${url}/api/get-cb-over?state=${region}&year=${year}`,
+	// 				config
+	// 			);
+	// 			if (response.status === 200) {
+	// 				setPredicted({
+	// 					x: Object.keys(years),
+	// 					y: response.data.predictedValue,
+	// 					type: "scatter",
+	// 				});
+	// 				console.log(response.data);
+	// 			}
+	// 		};
+	// 		if (currentModel === "CatBoost Regression") {
+	// 			getOneStateDataOverYearsCB();
+	// 		}
+	// 	} catch (error) {
+	// 		console.log(error.message);
+	// 	}
+	// }, [year, region, currentModel]);
 
 	// FOr country wise catBoost regression
 
 	// console.log(actual);
 	// console.log(predicted);
+
+	// const layout = {
+	// 	title: `Plastic Prediction for Year: ${year}-${year + 1}`,
+	// 	height: 600,
+	// 	width: 700,
+	// };
 	return (
 		<div>
 			<Header home={home} />
 			<div className="models">
 				<div className="models__dropdown">
 					<Dropdown setVariable={setYear} label={"Years"} labels={years} />
-					<Dropdown
+					{/* <Dropdown
 						setVariable={setRegion}
 						label={"Regions"}
 						labels={regions}
@@ -230,18 +269,13 @@ const Models = ({ home }) => {
 						setVariable={setCurrentModel}
 						label="Models"
 						labels={models}
-					/>
+					/> */}
 				</div>
 				<div className="models__data">
+					<LinePlot />
 					<Table row={rows} />
 					<div className="models__plot">
-						<LinePlot
-							model={currentModel}
-							label={"State Wise Prediction of "}
-							actual={actual}
-							predicted={predicted}
-							state={region}
-						/>
+						{/* <LinePlot data={[predicted]} layout={layout} /> */}
 
 						{/* <LinePlot
 							model={currentModel}
